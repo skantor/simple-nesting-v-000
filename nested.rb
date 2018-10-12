1b32ec06b325162be1d3f24a1749842d187be165
Sara Kantor <sara.kantor.nv@gmail.com>
2018-10-12 16:13:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  as_string = programmer_hash[dennis_ritchie][languages].to_s
+  as_string = programmer_hash[:dennis_ritchie][:languages].to_s
   as_string
 end
 
